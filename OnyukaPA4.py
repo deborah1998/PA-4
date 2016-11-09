0cd17a0b4869bae994ca92d109cd4047428ae3ac
deborahonyuka <daonyuka@loyola.edu>
2016-11-09 00:40:27 -0500

    PA 4 Design

--- OnyukaPA4.py
+++ OnyukaPA4.py
@@ -6,7 +6,7 @@ def read_in_file():
     while not os.path.exists(filename):
         filename = input("Please enter the file you would like to input")
 
-def process_file(infilename, outfilename):
+def process_file(infilename,outfilename):
     try:
         inputfile = open(infilename,"r")
         count = 0
@@ -14,6 +14,12 @@ def process_file(infilename, outfilename):
         for line in inputfile:
             status,survived,name,sex ,age ,embark,destination = line.split("|")
             if survived ==1:
+                count+=1
+                sum +=line
+                num_of_survivors = sum
+                print("The number  of people to survive tht titanic are"+num_of_survivors)
+                if status ==1:
+
 
 
 
