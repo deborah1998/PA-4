c2fa6632cee4a9e8829d627ffe4a4a2cd1a4e18a
deborahonyuka <daonyuka@loyola.edu>
2016-11-08 21:33:09 -0500

    PA 4 Design

--- OnyukaPA4.py
+++ OnyukaPA4.py
@@ -1,7 +1,27 @@
+import os
 
 
 def read_in_file():
-    input("Please enter the file you would like to input")
+    filename = input("Please enter the file you would like to input")
+    while not os.path.exists(filename):
+        filename = input("Please enter the file you would like to input")
+
+def process_file(inputfile, outputfile):
     try:
+        inputfile = open(filename, "r")
+        count = 0
+        sum = 0
+        for line in inputfile
 
 
+
+
+
+
+
+
+
+
+
+    except:
+        FileNotFoundError
