18bd898a7af208eaa898d5bd1dc131a03d60434e
deborahonyuka <daonyuka@loyola.edu>
2016-11-09 02:00:49 -0500

    PA 4 Design

--- OnyukaPA4.py
+++ OnyukaPA4.py
@@ -6,7 +6,7 @@ def read_in_file():
     while not os.path.exists(filename):
         filename = input("Please enter the file you would like to input")
 
-def process_file(infilename,outfilename):
+def process_file(infilename):
     try:
         inputfile = open(infilename,"r")
         num_of_survivors = 0
@@ -46,3 +46,10 @@ def process_file(infilename,outfilename):
         FileNotFoundError
     print("File does not exist")
     SystemExit(1)
+
+    def main():
+        print( "This program will determine the total amount of survivors on the titanic as well as the ")
+        process_file()
+        outfile_name=input("Please enter the the name of the outputfile")
+
+    main()
